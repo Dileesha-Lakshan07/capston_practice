668cecf8fdc93e1ced3f02c718cc8597a59a6a4d
DILEESHA\dlaks <dlakshansamaraweera@gmail.com>
2024-04-25 11:23:31 +0530

    change title again

--- dileesha/test_project_01/lib/main.dart
+++ dileesha/test_project_01/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home: const MyHomePage(title: 'dileesha Home Page'),
     );
   }
 }
