6fd49cf586c809dd16d17b4b34772ba25c4b5413
DESKTOP-SISGGQR\User <imandiu2000@gmail.com>
2024-05-10 22:23:17 +0530

    created a flutter profile page.

---
+++ Imandi/test_project_3/prfilepage/android/app/src/main/kotlin/com/example/prfilepage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.prfilepage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Imandi/test_project_3/prfilepage/lib/main.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  bool isObscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 184, 7, 228),
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          actions: [
+            Icon(
+              Icons.settings,
+              color: Colors.white,
+            )
+          ],
+          title: const Text(
+            "Profile",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Container(
+          padding: EdgeInsets.only(
+            left: 15,
+            top: 20,
+            right: 15,
+          ),
+          child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: ListView(
+              children: [
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 4, color: Colors.white),
+                          boxShadow: [
+                            BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                            )
+                          ],
+                          shape: BoxShape.circle,
+                          image: const DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ws1USoAXGs4liR7NSUmtkMgeamlY8ULcoU6rz_28lf2ZdZtaXe4svWTBYrzevU3MmBY&usqp=CAU',
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(width: 4, color: Colors.white),
+                              color: Color.fromARGB(
+                                  255, 184, 7, 228), // Fixed colors variable
+                            ),
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                          ))
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30),
+                buildTextField("Name", "Enter your name", false),
+                buildTextField("Age", "Enter your age", false),
+                buildTextField("Phone Number", "+94- - - - - - - - -", false),
+                buildTextField("Address", "Enter your Address", false),
+                buildTextField("Password", " _ _ _ _ _ _ _ _", true),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Text("Cancel",
+                          style: TextStyle(
+                              fontSize: 15,
+                              letterSpacing: 2,
+                              color: Colors.black)),
+                      style: OutlinedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(horizontal: 50),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Create",
+                          style: TextStyle(
+                              fontSize: 15,
+                              letterSpacing: 2,
+                              color: Colors.white)),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 184, 7, 228),
+                          padding: EdgeInsets.symmetric(horizontal: 50),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20))),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+          obscureText: isPasswordTextField ? isObscurePassword : false,
+          decoration: InputDecoration(
+              suffixIcon: isPasswordTextField
+                  ? IconButton(
+                      icon: Icon(
+                        Icons.remove_red_eye,
+                        color: Colors.grey,
+                      ),
+                      onPressed: () {})
+                  : null,
+              contentPadding: EdgeInsets.only(bottom: 5),
+              labelText: labelText,
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              hintText: placeholder,
+              hintStyle: TextStyle(
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
+              ))),
+    );
+  }
+}
